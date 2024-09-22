4bfbd75c715db848f2e950d221c284a6fdca6b6a
Checo894 <A01068505@tec.mx>
2024-09-22 10:32:34 -0600

    begin home

--- classes/Home.tsx
+++ classes/Home.tsx
@@ -7,7 +7,23 @@ export default function Home({navigation} : any) {
 
     return (
         <View style={styles.container}>
-            <Text>HOME</Text>
+            <View style={styles.search}>
+                <Image 
+                    source={require('../assets/search.svg')} 
+                    style={styles.icon1}
+                />
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder="Search"
+                />
+                <Image 
+                    source={require('../assets/mic.svg')} 
+                    style={styles.icon2}
+                />
+            </View>
+            {/* <View style={styles.recomended}></View>
+            <View style={styles.products}></View>
+            <View style={styles.tabBar}></View> */}
         </View>
     );
 }
@@ -19,5 +35,29 @@ const styles = StyleSheet.create({
         height: '100%',
         width: '100%',
         // alignItems: 'center',
+    },
+    search: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        // backgroundColor: '#0fa917',
+        // padding: 45,
+    },
+    searchInput: {
+        width: '70%',
+        height: 40,
+        borderColor: '878380',
+        backgroundColor: '#EDEEEF',
+        borderWidth: 1,
+        borderRadius: 30,
+    },
+    icon1: {
+        width: 30,
+        height: 30,
+    },
+    icon2: {
+        width: 30,
+        height: 30,
     },
 });
