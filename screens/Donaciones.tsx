5807bafcf4c13d28d68bf891195ec14bc127e093
alexeiddg <117191717+alexeiddg@users.noreply.github.com>
2024-10-09 18:11:26 -0600

    Productos / Donaciones Screen

--- screens/Donaciones.tsx
+++ screens/Donaciones.tsx
@@ -1,30 +1,76 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, ScrollView, ActivityIndicator, Image} from 'react-native';
+import { Product } from "../model/ProductModel";
+import { getProducts } from "../api/product/products.service";
 import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function Donaciones({ navigation }: any) {
-  const products = Array.from({ length: 6 }).map((_, index) => ({
-    id: index + 1,
-    title: 'Producto',
-    price: '$99.99',
-  }));
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const products = await getProducts();
+        setProducts(products);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Productos</Text>
+      {/* Products Grid */}
+      <ScrollView contentContainerStyle={styles.productsGrid}>
+        <Icon
+            name="arrow-back-outline"
+            size={24}
+            onPress={() => navigation.goBack()}
+            style={styles.backIcon}
+        />
+        <Text style={styles.title}>Productos</Text>
+        <Text style={styles.description}>
+          ¡Aquí encontrarás todos los productos que ofrece BAMX!
+          describir como funcionan las donaciones !!!
+        </Text>
+        {products.length > 0 ? (
+            products.map((product) => (
+                <View key={product.id} style={styles.productCard}>
+                  <TouchableOpacity
+                      onPress={() => navigation.navigate('ProductCard', { item: product })}
+                  >
+                    <Image
+                        source={{ uri: product.image_url }}
+                        style={styles.productImage}
+                    />
+                    <View style={styles.productInfo}>
+                      <View style={styles.productNameStock}>
+                        <Text style={styles.productName}>{product.name}</Text>
+                        <Text style={styles.productStock}>Stock: {product.stock_quantity}</Text>
+                      </View>
+                      <Text style={styles.productPrice}>MXN ${product.price}</Text>
+                      <Text style={styles.productDescription}>{product.description}</Text>
+                    </View>
+                  </TouchableOpacity>
 
-      <View style={styles.productsGrid}>
-        {products.map((product) => (
-          <TouchableOpacity
-            key={product.id}
-            style={styles.productCard}
-            onPress={() => navigation.navigate('ProductCard')}
-          >
-            <Text>{product.title}</Text>
-            <Text>{product.price}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
+                  <TouchableOpacity
+                      style={styles.addToCartButton}
+                      onPress={() => {
+                        //TODO: Add to cart logic here
+                      }}
+                  >
+                    <Text style={styles.addToCartText}>Añadir al carrito</Text>
+                  </TouchableOpacity>
+                </View>
+            ))
+        ) : (
+            <ActivityIndicator size="large" color="#0000ff" />
+        )}
+      </ScrollView>
 
       <View style={styles.tabBar}>
         <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.homeIconContainer}>
@@ -45,20 +91,71 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20,
+    marginTop: -12,
+    marginBottom: 12,
   },
   productsGrid: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
+    paddingBottom: 20,
   },
   productCard: {
-    width: '45%',
-    backgroundColor: '#f0f0f0',
-    padding: 20,
+    width: '100%',
+    backgroundColor: '#fff',
     borderRadius: 10,
+    padding: 15,
+    marginBottom: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+  },
+  productImage: {
+    width: '100%',
+    height: 200,
+    borderRadius: 8,
+    resizeMode: 'cover',
     marginBottom: 10,
+  },
+  productInfo: {
+    alignItems: 'flex-start',
+  },
+  productNameStock: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginBottom: 5,
+  },
+  productName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  productStock: {
+    fontSize: 14,
+    color: '#555',
+  },
+  productPrice: {
+    fontSize: 16,
+    color: '#f31f35',
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  productDescription: {
+    fontSize: 14,
+    color: '#888',
+    marginBottom: 10,
+  },
+  addToCartButton: {
+    backgroundColor: '#e11d2f',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 8,
     alignItems: 'center',
+    marginTop: 10,
+  },
+  addToCartText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
   },
   tabBar: {
     position: 'absolute',
@@ -72,10 +169,22 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
+    paddingVertical: 28,
   },
   homeIconContainer: {
     backgroundColor: '#f31f35',
     padding: 10,
     borderRadius: 50,
   },
+  backIcon: {
+    marginBottom: 20,
+  },
+  description: {
+    fontSize: 18,
+    marginBottom: 24,
+    fontWeight: '600',
+    textAlign: 'center',
+  },
 });
+
+
--- screens/ProductCard.tsx
+++ screens/ProductCard.tsx
@@ -1,23 +1,83 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-export default function ProductCard({ navigation }: any) {
+export default function ProductCard({ route, navigation }: any) {
+  const { item } = route.params;
+  const isProduct = item && typeof item === 'object' && 'price' in item;
+
   return (
-    <View style={styles.container}>
-      <Icon name="arrow-back-outline" size={24} onPress={() => navigation.goBack()} style={styles.backIcon} />
-      <View style={styles.productImageContainer}>
-        <Text>Image Slider Placeholder</Text>
+      <View style={styles.container}>
+        <Icon
+            name="arrow-back-outline"
+            size={24}
+            onPress={() => navigation.goBack()}
+            style={styles.backIcon}
+        />
+        <View style={styles.productImageContainer}>
+          {item && item.image_url ? (
+              <Image source={{ uri: item.image_url }} style={styles.productImage} />
+          ) : (
+              <Text>Image Slider Placeholder</Text>
+          )}
+        </View>
+        <View style={styles.productDetails}>
+          {item ? (
+              <>
+                <Text style={styles.productTitle}>{item.name}</Text>
+                {/* Conditionally render product or reward attributes */}
+                {isProduct ? (
+                    <>
+                      <Text style={styles.productPrice}>MXN ${item.price}</Text>
+                      <Text style={styles.productDescription}>
+                        Stock disponible: {item.stock_quantity}
+                      </Text>
+                    </>
+                ) : (
+                    <>
+                      <Text style={styles.rewardPoints}>
+                        Costo en puntos: {item.points_cost} puntos
+                      </Text>
+                    </>
+                )}
+
+                <Text style={styles.productDescription}>
+                  {item.description}
+                </Text>
+
+                {isProduct ? (
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => {
+                          //TODO: Add to cart logic here
+                        }}
+                    >
+                      <Text
+                          style={{color: '#fff', fontWeight: 'bold', fontSize: 16}}
+                      >
+                        Añadir al carrito
+                      </Text>
+                    </TouchableOpacity>
+                ) : (
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => {
+                          //TODO: Buy logic here
+                        }}
+                    >
+                      <Text
+                          style={{color: '#fff', fontWeight: 'bold', fontSize: 16}}
+                      >
+                        Canjear con puntos
+                      </Text>
+                    </TouchableOpacity>
+                )}
+              </>
+          ) : (
+              <Text style={styles.loadingText}>Cargando...</Text>
+          )}
+        </View>
       </View>
-      <View style={styles.productDetails}>
-        <Text style={styles.productTitle}>Producto</Text>
-        <Text style={styles.productPrice}>$99.99</Text>
-        <Text style={styles.productDescription}>
-          Descripcion: ksdalkamdklmdsalfkmdsflkdsamfdslkfmnlsdkfmdslkfmladskfmladkmfladkmfladkmfldskmfklmekdfnafkefnoewqfmklwesmdfloawsenfkaewmflkds
-        </Text>
-        <Button title="Añadir al carrito" color="#f31f35" onPress={() => {}} />
-      </View>
-    </View>
   );
 }
 
@@ -36,6 +96,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  productImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
   productDetails: {
     marginTop: 20,
   },
@@ -45,9 +110,30 @@ const styles = StyleSheet.create({
   },
   productPrice: {
     fontSize: 20,
-    marginVertical: 10,
+    marginVertical: 8,
+    color: '#f31f35',
+  },
+  rewardPoints: {
+    fontSize: 20,
+    marginVertical: 8,
+    color: '#f31f35',
   },
   productDescription: {
-    marginBottom: 20,
+    marginBottom: 8,
+    fontSize: 16,
+  },
+  loadingText: {
+    fontSize: 18,
+    color: '#878380',
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  button: {
+    backgroundColor: '#e11d2f',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
